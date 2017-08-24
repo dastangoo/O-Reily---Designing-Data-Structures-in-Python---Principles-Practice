68071d6840afc43230a455261ab075b75cfee767
dastangoo <hamed.dastangoo@gmail.com>
2017-08-24 08:05:03 +0430

    Some tweaks

--- bigO.py
+++ bigO.py
@@ -3,6 +3,6 @@ import timeit
 
 print('N\tSum Time')
 for trial in [2**_ for _ in range(1,9)]:
-    numbers = [random.randint(1,9) for_ in range(trial)]
+    numbers = [random.randint(1,9) for _ in range(trial)]
     m = timeit.timeit(stmt='sum=0\nfor d in numbers:\n\tsum', setup='import random\nnumbers = ' + str(numbers))
     print('{0:d}\t{1:f}'.format(trial, m))
