c358e2b4d6b8c0706021fe04261b9121fb5bc4de
dastangoo <hamed.dastangoo@gmail.com>
2017-08-24 08:02:11 +0430

    Analysis Techniques

---
+++ bigO.py
@@ -0,0 +1,8 @@
+import random
+import timeit
+
+print('N\tSum Time')
+for trial in [2**_ for _ in range(1,9)]:
+    numbers = [random.randint(1,9) for_ in range(trial)]
+    m = timeit.timeit(stmt='sum=0\nfor d in numbers:\n\tsum', setup='import random\nnumbers = ' + str(numbers))
+    print('{0:d}\t{1:f}'.format(trial, m))
